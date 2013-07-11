43c32e5973fc49a100fba7f615063a34460c1f30
tuandq@lifetimetech.vn <tuandq@lifetimetech.vn>
2013-07-11 15:03:54 +0700

    Init project opencart

---
+++ admin/config - window-bak.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/thucpham/admin/');
+define('HTTP_CATALOG', 'http://localhost/thucpham/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/thucpham/admin/');
+define('HTTPS_CATALOG', 'http://localhost/thucpham/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\webserver\htdocs\thucpham/admin/');
+define('DIR_SYSTEM', 'F:\webserver\htdocs\thucpham/system/');
+define('DIR_DATABASE', 'F:\webserver\htdocs\thucpham/system/database/');
+define('DIR_LANGUAGE', 'F:\webserver\htdocs\thucpham/admin/language/');
+define('DIR_TEMPLATE', 'F:\webserver\htdocs\thucpham/admin/view/template/');
+define('DIR_CONFIG', 'F:\webserver\htdocs\thucpham/system/config/');
+define('DIR_IMAGE', 'F:\webserver\htdocs\thucpham/image/');
+define('DIR_CACHE', 'F:\webserver\htdocs\thucpham/system/cache/');
+define('DIR_DOWNLOAD', 'F:\webserver\htdocs\thucpham/download/');
+define('DIR_LOGS', 'F:\webserver\htdocs\thucpham/system/logs/');
+define('DIR_CATALOG', 'F:\webserver\htdocs\thucpham/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'thucpham');
+define('DB_PREFIX', 'oc_');
+?>
